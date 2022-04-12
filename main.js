514376780c63047b6d5c4797099ea3ff0d04da40
Thierry Maignan <maignan.thierry@outlook.fr>
2022-04-12 15:42:14 +0200

    added animation on scroll

---
+++ main.js
@@ -0,0 +1,22 @@
+const ratio = .3
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+function handleIntersect(entries, observer){
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio){
+            console.log("visible")
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+      
+    })
+}
+
+const observer = new IntersectionObserver(handleIntersect,options)
+document.querySelectorAll('.reveal').forEach(function (r){
+    observer.observe(r)
+})
